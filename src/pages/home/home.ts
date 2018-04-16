8716e94c73077c821bf61aacba28eebd92245e8a
Nicth Giraldo <nicth.giraldo@gmail.com>
2018-04-16 09:46:54 -0500

    primerCommit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales'; 
+import { Animal } from '../../interfaces/animal.interface';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales:Animal[] = [];
 
+  constructor() {
+
+    this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir( animal:Animal ){
+    console.log( animal )
+    let audio = new Audio();
+    audio.src= animal.audio;
+    audio.load();
+    audio.play();
+    animal.reproduciendo = true;
+    setTimeout(()=>animal.reproduciendo = false, animal.duracion * 1000);
   }
 
 }
